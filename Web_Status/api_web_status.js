8ddc7c8b38473ae76251f4b9ca912e231b4565a8
Grimm <85710701+GrimmHX@users.noreply.github.com>
2022-04-26 19:11:48 -0400

    Release

---
+++ Web_Status/api_web_status.js
@@ -0,0 +1,13 @@
+(function() {
+
+    code = 400
+
+    if (code == "400") {
+        return;
+    }
+
+    if (code == "402") {
+        window.location.replace("https://pittimes.netlify.app/coming-soon");
+    }
+    
+})()
