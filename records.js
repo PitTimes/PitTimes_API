6d7485c53eecb16b748ea821f569914d7b4637cd
Kris Powers <85710701+KrisPowers@users.noreply.github.com>
2022-05-22 18:17:55 -0400

    Update records.js

--- records.js
+++ records.js
@@ -21,7 +21,7 @@ var WP3 = ['Sebastian','Vettel',AstonC,'Red Bull Racing/Ferrari/Aston Martin',53
 // Consecutive Wins
 var CWP1 = ['Sebastian','Vettel',RedBullC,'Red Bull Racing',9,"0"]
 var CWP2 = ['Alberto','Ascari',FerrariC,'Ferrari',7,"GapEX"]
-var CWP3 = ['Micheal','Schumacher',FerrariC,'Ferrari7',7,"GapEX"]
+var CWP3 = ['Micheal','Schumacher',FerrariC,'Ferrari',7,"GapEX"]
 
 // Record Wins Gap Calc
 var Gap = WP1[4] - WP2[4]
@@ -47,5 +47,5 @@ var CPWP1 = `<div class="col-md-50 d-flex align-items-stretch"> <a class="icon-b
 var CPWP2 = `<div class="col-md-50 d-flex align-items-stretch"> <a class="icon-box1"> <span class="rank">2nd</span> <span class="team-border" style="background: ${CWP2[2]}; color: ${CWP2[2]}; border-radius: 2px;">|</span> <span class="name">${CWP2[0]} <strong>${CWP2[1]}</strong></span> <span class="d-team">${CWP2[3]}</span> <span class="points" style="width: 65px;">${CWP2[4]} Wins</span> <span class="gap" style="width: 95px;">GAP: ${CWP2[5]} Wins</span> </a> </div>`
 var CPWP3 = `<div class="col-md-50 d-flex align-items-stretch"> <a class="icon-box1"> <span class="rank">3rd</span> <span class="team-border" style="background: ${CWP3[2]}; color: ${CWP3[2]}; border-radius: 2px;">|</span> <span class="name">${CWP3[0]} <strong>${CWP3[1]}</strong></span> <span class="d-team">${CWP3[3]}</span> <span class="points" style="width: 65px;">${CWP3[4]} Wins</span> <span class="gap" style="width: 95px;">GAP: ${CWP3[5]} Wins</span> </a> </div>`
 
-var content = `<div class="title2"> <h1>All-Time Wins Leaderboard<t>.</t><br></h1> <h2>Up-to-date Formula 1 Standings for All-Time Wins</h2> <P1> ${PWP1} </P1> <P2> ${PWP2} </P2> <P3> ${PWP3} </P3></div><div class="title2"> <h1>Consecutive Wins Leaderboard<t>.</t><br></h1> <h2>Up-to-date Formula 1 Standings for Most Consecutive Wins</h2> <P1> ${PWP1} </P1> <P2> ${PWP2} </P2> <P3> ${PWP3} </P3></div>`
+var content = `<div class="title2"> <h1>All-Time Wins Leaderboard<t>.</t><br></h1> <h2>Up-to-date Formula 1 Standings for All-Time Wins</h2> <P1> ${PWP1} </P1> <P2> ${PWP2} </P2> <P3> ${PWP3} </P3></div><div class="title2"> <h1>Consecutive Wins Leaderboard<t>.</t><br></h1> <h2>Up-to-date Formula 1 Standings for Most Consecutive Wins</h2> <P1> ${CPWP1} </P1> <P2> ${CPWP2} </P2> <P3> ${CPWP3} </P3></div>`
 document.getElementsByTagName("Choice")[0].innerHTML = content
