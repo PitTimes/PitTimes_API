1a6435f2dedad4c5f1fbb478f524e1a3597b85fa
Kris Powers <85710701+KrisPowers@users.noreply.github.com>
2023-01-05 14:11:27 -0500

    v2.0.0 ACTIVE DEVELOPMENT

---
+++ index.js
@@ -0,0 +1,29 @@
+const drivers = {
+    "Max": {
+      "firstName": "Max",
+      "lastName": "Verstappen",
+      "number": 1,
+      "team": "Red Bull Racing",
+      "nationality": "NED",
+      "img": null
+      "points": 454,
+    },
+    "Sergio": {
+      "firstName": "Sergio",
+      "lastName": "Perez",
+      "number": 11,
+      "team": "Red Bull Racing",
+      "nationality": "MEX",
+      "img": null
+      "points": 305,
+    }
+}
+
+// Sorts Drivers Standings (By Points)
+const standigs_points = {}
+
+for(i in drivers){
+  
+  
+  
+}
